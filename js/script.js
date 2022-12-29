258a14bcf3aea1ddb0e1b3c4d26f2433092eff31
Joseph Charles <112246775+jccdevweb@users.noreply.github.com>
2022-12-29 13:10:38 +0800

    Implement Single page Portfolio

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 
 const navBtn = document.querySelector('#navbar-toggler');
 const navDiv = document.querySelector('.navbar-collapse');
+const navItem = document.querySelectorAll('.nav-item');
 
+console.log(navItem)
 navBtn.addEventListener('click', () => {
     navDiv.classList.toggle('showNav');
 });
@@ -15,3 +17,37 @@ window.addEventListener('resize', () => {
         document.body.classList.remove('resize-animation-stopper');
     }, 400);
 });
+
+navItem.forEach((nav) => {
+    nav.addEventListener('click', ()=>{
+        navDiv.classList.toggle('showNav');
+    })
+ 
+})
+// modal start
+const btns = document.querySelectorAll("[data-target]");
+const close_modals = document.querySelectorAll(".close-modal");
+const overlay = document.getElementById("overlay");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(btn.dataset.target).classList.add("active");
+    overlay.classList.add("active");
+  });
+});
+
+close_modals.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const modal = btn.closest(".modal");
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+});
+
+window.onclick = (event) => {
+  if (event.target == overlay) {
+    const modals = document.querySelectorAll(".modal");
+    modals.forEach((modal) => modal.classList.remove("active"));
+    overlay.classList.remove("active");
+  }
+};
